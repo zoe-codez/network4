ca31dfeb0e81fa305fd6bcf931dd737110ada85d
Zoe Codez <github@zoe-codez.tools>
2024-06-18 23:12:28 -0500

    upgrade deps

--- src/pando/controllers/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./backup.controller";
-export * from "./init.controller";
-export * from "./misc.controller";
-export * from "./system.controller";
--- src/pando/helpers/constants.ts
+++
@@ -1,7 +0,0 @@
-export const PANDO_DATABASE = "PANDO_DATABASE";
-
-/**
- * Nextcloud caldav
- */
-export const CALENDAR_UPDATE = "CALENDAR_UPDATE";
-export const BACKUP_EVENT = "BACKUP_EVENT";
